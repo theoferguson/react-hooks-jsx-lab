3ef4d34c6135f372db0c0edb4f133cbb0d4337ee
Theo Ferguson <ferguson.theo@gmail.com>
2021-08-31 12:03:52 -0400

    commit tree e75e1a66870eec3ef526cfcde23dc69f19282300

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,13 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  const headingStyle = {
+    color: 'firebrick'
+  };
+  return <div id="home">
+    <h1 style={headingStyle}>{ name } is a Web Developer from { city }</h1>
+
+  </div>;
 }
 
 export default Home;
